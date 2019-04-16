15d1a99289470207459698f390c5c04e549bb48d
sanqiushu <sanqiushugyh@qq.com>
2019-04-16 20:29:52 +0800

    第一阶段的admin定制

--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -56,7 +56,7 @@ class SideBar(models.Model):
     content = models.CharField(max_length=500, blank=True, verbose_name='内容', help_text='如果设置的不是HTML类型，可为空')
     status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name='状态')
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.DO_NOTHING)
-    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     class Meta:
         verbose_name = verbose_name_plural = '侧边栏'
